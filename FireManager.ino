0a969b8adfc1ed604ed9defb7d237f83fa800b0f
Kaz Walker <me@kaz.codes>
2019-08-08 00:06:18 -0600

    Initial commit.

---
+++ FireManager.ino
@@ -0,0 +1,32 @@
+// Project includes
+#include "globals.h"
+#include "power.h"
+#include "networking.h"
+#include <max6675.h>
+
+int ktcSO = D6;
+int ktcCS = D6;
+int ktcCLK = D5;
+
+MAX6675 ktc(ktcCLK, ktcCS, ktcSO);
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+  Serial.println("");
+  Serial.println("Booted.");
+
+  setupPower();
+  setupNetworking();
+
+  delay(500);
+}
+
+void loop()
+{
+  Serial.print("C = ");
+  Serial.print(ktc.readCelsius());
+  delay(1000);
+}
---
+++ power.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <Arduino.h>
+#include "globals.h"
+
+void setupPower();
+void forceNapIf(bool force);
